e341d5ffb0e9cd0102fd5648b1c6d93046f21101
HidAndrei <andreiminea1810@gmail.com>
2025-12-31 15:51:34 +0200

    Adaugat proiect initial Vite + React

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import chenar from './assets/chenar.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="chenar-container">
+        <img src={chenar} className="chenar-img" alt="chenar" />
+        <p className="chenar-text">Pizza Boy</p>
+        <p className="chenar-stats">Skill {count}/20</p>
+        <button className="chenar-button" onClick={() => setCount((count) => count + 1)}>+</button>
+      </div>
+    </>
+  )
+}
+
+export default App
